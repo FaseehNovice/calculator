e460ec8f708f8fe3dd0fe0d12edbf31d55e2d516
FaseehNovice <faseehurrehman499@gmail.com>
2025-09-13 03:54:39 -0700

    js basic arithmetic  operations

--- javascript.js
+++ javascript.js
@@ -1,1 +1,32 @@
-console.log("Hello World!")
+function add( a , b ){
+    return a + b;
+}
+function subtract(a , b){
+    return a - b;
+}
+function multiply(a , b){
+    return a * b;
+}
+function divide(a,b){
+    return a / b;
+}
+
+function operate( a , opr , b){
+    switch(opr){
+        case '+':
+            return add(a,b);
+            break;
+        case '-':
+            return subtract(a,b);
+            break;
+        case '*':
+            return multiply(a,b);
+            break;
+        case '/':
+            return divide(a,b);
+            break;
+        default:
+            console.log("SYNTAX ERROR!")
+    }
+}
+console.log(operate(2 ,'+', 3));
