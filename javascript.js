59db6153a8f684a2926741d5f1b6f5055e346c9c
FaseehNovice <faseehurrehman499@gmail.com>
2025-09-13 07:12:16 -0700

    basic js logic to compute arithmetic

--- javascript.js
+++ javascript.js
@@ -29,4 +29,46 @@ function operate( a , opr , b){
             console.log("SYNTAX ERROR!")
     }
 }
-console.log(operate(2 ,'+', 3));
+btns = document.querySelectorAll("button");
+displayScreen = document.querySelector("#display-screen")
+
+let prev = null , curr = null , res = null , opr = '';
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        let value = btn.textContent;
+
+        if (!isNaN(value)) {
+            if (curr === null) curr = value;
+            else if(curr == res) displayScreen.textContent = "";
+            else curr += value; 
+            displayScreen.textContent = curr;
+        }
+
+        else if (["+", "-", "*", "/"].includes(value)) {
+            if (curr !== null) {
+                prev = parseInt(curr);
+                curr = null;
+                opr = value;
+                displayScreen.textContent = opr;
+            }
+        }
+
+        else if (value === "=") {
+            if (prev !== null && curr !== null && opr !== '') {
+                res = operate(prev, opr, parseInt(curr));
+                res = parseFloat(res.toFixed(1));
+                displayScreen.textContent = res;
+                curr = res;  
+                prev = null;
+                opr = '';
+            }
+        }
+
+        else if (value === "C") {
+            displayScreen.textContent = "";
+            prev = curr = res = null;
+            opr = '';
+        }
+    });
+});
